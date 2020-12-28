9778c75a1a798cd6517c004c1c8b5edf443a538a
dominikNITA <dominik.nita@gmail.com>
2020-12-28 03:12:00 +0100

    Config fix

---
+++ cmake-build-debug/CMakeFiles/AgeOfWar.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "AgeOfWar"
+  "AgeOfWar.pdb"
+  "CMakeFiles/AgeOfWar.dir/main.cpp.o"
+  "CMakeFiles/AgeOfWar.dir/src/players/ComputerPlayer.cpp.o"
+  "CMakeFiles/AgeOfWar.dir/src/players/HumanPlayer.cpp.o"
+  "CMakeFiles/AgeOfWar.dir/src/players/IPlayer.cpp.o"
+  "CMakeFiles/AgeOfWar.dir/src/units/Archer.cpp.o"
+  "CMakeFiles/AgeOfWar.dir/src/units/Catapult.cpp.o"
+  "CMakeFiles/AgeOfWar.dir/src/units/Fantassin.cpp.o"
+  "CMakeFiles/AgeOfWar.dir/src/units/IAttackable.cpp.o"
+  "CMakeFiles/AgeOfWar.dir/src/units/IUnit.cpp.o"
+  "CMakeFiles/AgeOfWar.dir/src/units/SuperSoldier.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AgeOfWar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
